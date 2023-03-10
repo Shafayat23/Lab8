9ab27a67103e7c59a66130f5c0faf3b8dd9ed237
Shafayat23 <sadman.shafayat023@gmail.com>
2023-03-09 18:51:16 -0700

    Test fail has city

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -54,7 +54,10 @@ public class CustomList extends ArrayAdapter<City> {
 
     }
     boolean hasCity(City city) {
-        
+        if (cities.contains(city)){
+            return true;
+        }
+        return false;
     }
 
 
